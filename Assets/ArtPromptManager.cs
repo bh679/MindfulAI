0fb21b64933c71657dc8f7c9b8e365b41c1e98b6
Brennan Hatton <brennan@brennanhatton.com>
2023-04-20 17:19:04 +0800

    Making it ask questions and offer wisdom

---
+++ Assets/ArtPromptManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BrennanHatton.AI;
+
+
+
+public class ArtPromptManager : MonoBehaviour
+{
+	public MsCogVoiceToGPT GPTGen;
+	
+	public int paintingId = 0, questionId = 0;
+	
+	public string[] PaintingPersonality,
+		Questions,
+		Wisdom;
+	
+	void Reset()
+	{
+		GPTGen = this.GetComponent<MsCogVoiceToGPT>();
+	}
+	
+	void Start()
+	{
+		AskQuestion();
+	}
+	
+	public void AskQuestion()
+	{
+		GPTGen.promptWrapper.prePrompt = PaintingPersonality[paintingId]+Questions[questionId];
+		
+		questionId = (questionId + 1) % Questions.Length;
+		
+	}
+}
